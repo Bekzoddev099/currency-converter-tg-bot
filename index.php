24cc773ab7cc2c7fbc03ead2c6a3b56380ec2d0a
Bekzod Uskanboyev <uskanboyevbekzod@gmail.com>
2024-07-14 04:30:16 +0500

    second commit

--- index.php
+++ index.php
@@ -3,12 +3,80 @@
 declare(strict_types=1);
 
 require 'vendor/autoload.php';
+require 'savedate.php';
+
+use GuzzleHttp\Client;
+
+$token = "7430675498:AAFFHNpKebcZ-rvwTnsABXrDeAVzSpS5WBI";
+$tgApi = "https://api.telegram.org/bot$token/";
+
+$client = new Client(['base_uri' => $tgApi]);
 
 $update = json_decode(file_get_contents('php://input'));
 
-if (isset($update)) {
-    require 'bot.php';
-    return;
+$keyboard = [
+    'inline_keyboard' => [
+        [
+            ['text' => '🇺🇸 <=> 🇺🇿', 'callback_data' => 'usd2uzs'],
+            ['text' => '🇺🇿 <=> 🇺🇸', 'callback_data' => 'uzs2usd'],
+        ],
+    ]
+];
+
+
+$saveuser = new SaveUsersData();
+
+if (isset($update->message)) 
+{
+    $message = $update->message;
+    $chat_id = $message->chat->id;
+    $text = $message->text;
+
+    if ($text === '/start') 
+    {
+        $client->post('sendMessage', [
+            'form_params' => [
+                'chat_id' => $chat_id,
+                'text' => 'Select an exchange rate',
+                'reply_markup' => json_encode($keyboard)
+            ]
+        ]);
+    }
+
+    if (is_numeric($text)) 
+    {
+        $conversionType = $saveuser->sendConvertionType($chat_id);
+        if ($conversionType) {
+            $saveuser->allusersinfo($chat_id, $conversionType, (float)$text);
+            $client->post('sendMessage', [
+                'form_params' => [
+                    'chat_id' => $chat_id,
+                    'text' => $saveuser->getuser((float)$text, $chat_id)
+                ]
+            ]);
+        } else {
+            $client->post('sendMessage', [
+                'form_params' => [
+                    'chat_id' => $chat_id,
+                    'text' => 'Error: No conversion type found.'
+                ]
+            ]);
+        }
+    }
+
 }
 
-require 'view/dashboard.php';
+if (isset($update->callback_query)) {
+    $callback_query = $update->callback_query;
+    $chat_id        = $callback_query->message->chat->id;
+    $callback_data  = $callback_query->data;
+
+    $saveuser->saveuser($chat_id, $callback_data);
+
+    $client->post('sendMessage', [
+        'form_params' => [
+            'chat_id' => $chat_id,
+            'text' => 'Enter the amount:'
+        ]
+    ]);
+}
